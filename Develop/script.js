ac634e3f260970cf495474c6a2ffa9fee780b1dd
Tony Mai <tonay6@gmail.com>
2022-10-30 18:08:43 -0500

    final product

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+
 function generatePassword() {
 
   var userInput = window.prompt('How long do you want your password to be')
@@ -15,7 +28,7 @@ function generatePassword() {
     return
   }
 
-  if (passwordLength < 8 | | passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     windown.alert("Password length must be between 8 and 128 characters")
     return
   }
@@ -30,10 +43,42 @@ function generatePassword() {
   var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v","w", "x", "y", "z"]
   var uppercaseList = []
 
+  var optionsCart = []
+
   for (var i = 0; i < lowercaseList.length; i++) {
     uppercaseList[i] = lowercaseList[i].toUpperCase()
   }
 
+  if (userWantsNumbers === true) {
+    optionsCart.push(numberList)
+  }
+
+  if (userWantsSymbols === true) {
+    optionsCart.push(symbolList)
+  }
+
+  if (userWantsLowercase === true) {
+    optionsCart.push(lowercaseList)
+  }
+
+  if (userWantsUppercase === true) {
+    optionsCart.push(uppercaseList)
+  }
+
+  if (optionsCart.length === 0) {
+    optionsCart.push(lowercaseList)
+  }
+
+
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandomItem(optionsCart)
+    var randomChar = getRandomItem(randomList)
+    generatedPassword += randomChar
+  }
+
+  return generatedPassword
 }
 
 
