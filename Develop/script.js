cf2f2078d51cec9b516dc7cb156cdbce899563ca
Tony Mai <tonay6@gmail.com>
2022-10-30 14:23:30 -0500

    intial script.js

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,40 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var userInput = window.prompt('How long do you want your password to be')
+  
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength))  {
+    window.alert("That's not a number!")
+    return
+  }
+
+  if (passwordLength < 8 | | passwordLength > 128) {
+    windown.alert("Password length must be between 8 and 128 characters")
+    return
+  }
+
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase in your password?")
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v","w", "x", "y", "z"]
+  var uppercaseList = []
+
+  for (var i = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
+  }
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
